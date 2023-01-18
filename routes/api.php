0b55d51b14fdf9633407995d40a98ce26bca91b3
新島　駿 <niijima131229@docomo.ne.jp>
2023-01-18 12:11:53 +0900

    middlwareをapi.phpに移し、未ログイン時のredirect先を変更

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::controller(AuthController::class)->group(function () {
-	Route::post('login', 'login');
-	Route::post('register', 'register');
-	Route::post('logout', 'logout');
-	Route::post('refresh', 'refresh');
+
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::middleware('auth:api')->controller(AuthController::class)->group(function () {
+	Route::post('logout', 'logout')->name('logout');
+	Route::post('refresh', 'refresh')->name('refresh');
 });
