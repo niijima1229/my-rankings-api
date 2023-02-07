a0a408646162e726d0a7b9a770790bed596d4bbd
新島　駿 <niijima131229@docomo.ne.jp>
2023-02-07 21:07:47 +0900

    jwtをcookieにも保存するよう設定

--- web/app/Http/Controllers/AuthController.php
+++ web/app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Cookie\CookieJar;
 use App\Models\User;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
@@ -14,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthController extends Controller
 {
 
-	public function login(LoginRequest $request): JsonResponse
+	public function login(LoginRequest $request, CookieJar $cookie): JsonResponse
 	{
 		$credentials = $request->safe()->only('email', 'password');
 		$token = Auth::attempt($credentials);
@@ -33,7 +34,16 @@ class AuthController extends Controller
 				'token' => $token,
 				'type' => 'bearer',
 			]
-		], Response::HTTP_OK);
+		], Response::HTTP_OK)
+			->withCookie($cookie->make(
+				'token',
+				$token,
+				config('jwt.refresh_ttl'), // minutes
+				'/',
+				config('app.front_url'), // domain
+				$request->getScheme() === 'https', // secure
+				true // httpOnly
+			));;
 	}
 
 	public function register(RegisterRequest $request): JsonResponse
