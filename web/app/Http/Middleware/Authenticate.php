8c1ed82614ea40598d5b60af5ce2ca6319884d90
新島　駿 <niijima131229@docomo.ne.jp>
2023-03-07 12:19:04 +0900

    authの判定をシンプルにして、ヘッダー重複のエラーを解消

--- web/app/Http/Middleware/Authenticate.php
+++ web/app/Http/Middleware/Authenticate.php
@@ -2,24 +2,28 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class Authenticate extends Middleware
+class Authenticate
 {
 	/**
-	 * Get the path the user should be redirected to when they are not authenticated.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return string|null
-	 */
-	protected function redirectTo($request)
-	{
-		if (!$request->expectsJson()) {
-			return response()->json([
-				'status' => 'error',
-				'message' => 'Unauthorized',
-			], Response::HTTP_UNAUTHORIZED);
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+		if (Auth::check()) {
+			return $next($request);
 		}
-	}
+		return response()->json([
+			'status' => 'error',
+			'message' => 'Unauthorized',
+		], Response::HTTP_UNAUTHORIZED);
+    }
 }
